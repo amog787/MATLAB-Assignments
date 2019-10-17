cd144fa3e40350834ddd3ac3a271e8d21b4a95a6
amog787 <amoghmaiya@gmail.com>
2019-10-17 21:24:18 +0530

    prog 5

---
+++ pgm5.m
@@ -0,0 +1,54 @@
+% 5.Perform Image compression using JPEG
+
+I1 = imread('C:\mat\chicken.jpg');
+I = I1(:,:,1);
+I = im2double(I);
+T = dctmtx(8);
+B = blkproc(I,[8 8],'P1*x*P2',T,T');
+mask = [1   1   1   1   0   0   0   0
+        1   1   1   0   0   0   0   0
+        1   1   0   0   0   0   0   0
+        1   0   0   0   0   0   0   0
+        0   0   0   0   0   0   0   0
+        0   0   0   0   0   0   0   0
+        0   0   0   0   0   0   0   0
+        0   0   0   0   0   0   0   0];
+B2 = blkproc(B,[8 8],'P1.*x',mask);
+I2 = blkproc(B2,[8 8],'P1*x*P2',T',T);
+
+I = I1(:,:,2);
+I = im2double(I);
+T = dctmtx(8);
+B = blkproc(I,[8 8],'P1*x*P2',T,T');
+mask = [1   1   1   1   0   0   0   0
+        1   1   1   0   0   0   0   0
+        1   1   0   0   0   0   0   0
+        1   0   0   0   0   0   0   0
+        0   0   0   0   0   0   0   0
+        0   0   0   0   0   0   0   0
+        0   0   0   0   0   0   0   0
+        0   0   0   0   0   0   0   0];
+B2 = blkproc(B,[8 8],'P1.*x',mask);
+I3 = blkproc(B2,[8 8],'P1*x*P2',T',T);
+
+
+I = I1(:,:,3);
+I = im2double(I);
+T = dctmtx(8);
+B = blkproc(I,[8 8],'P1*x*P2',T,T');
+mask = [1   1   1   1   0   0   0   0
+        1   1   1   0   0   0   0   0
+        1   1   0   0   0   0   0   0
+        1   0   0   0   0   0   0   0
+        0   0   0   0   0   0   0   0
+        0   0   0   0   0   0   0   0
+        0   0   0   0   0   0   0   0
+        0   0   0   0   0   0   0   0];
+B2 = blkproc(B,[8 8],'P1.*x',mask);
+I4 = blkproc(B2,[8 8],'P1*x*P2',T',T);
+
+
+L(:,:,:)=cat(3,I2, I3, I4);
+
+subplot(1,2,1);imshow(I1);title('Original Image');
+subplot(1,2,2);imshow(L);title('Compressed Image');
